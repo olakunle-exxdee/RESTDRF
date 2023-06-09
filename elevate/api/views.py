f322e6ec0eab553fa2d67259b7e160344779519a
Olakunle Ogunsolu <saheedogunsolu2016@gmail.com>
2023-06-09 14:36:50 +0200

    first commit

---
+++ elevate/api/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializers
+from rest_framework.decorators import api_view
+
+# Create your views here.
+
+
+@api_view(["GET", "POST"])
+def product_list(request):
+    pass
+
+    if request.method == "GET":
+        products = Product.objects.all()
+
+        # serialized the product
+        serializer = ProductSerializers(products, many=True)
+
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializers(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+        return Response(serializer.data)
+
+
+@api_view(["GET", "P"])
+def single_product(request, pk):
+    pass
+
+    if request.method == "GET":
+        products = Product.objects.get(id=pk)
+
+        # serialized the product
+        serializer = ProductSerializers(products, many=False)
+
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializers(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+        return Response(serializer.data)
